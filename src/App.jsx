4f58f4f9ed6ff9fdfa53af449b24099f397b72c1
Daniel X <dxu@coldfix.dev>
2023-03-04 16:29:23 -0600

    Added React Helmet and CDN link

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { parse } from 'marked';
+import { Helmet } from 'react-helmet';
 import DOMPurify from 'dompurify';
 import './App.scss'
 
@@ -12,6 +13,9 @@ function App() {
 
   return (
     <div className="App">
+      <Helmet>
+        <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js" />
+      </Helmet>
       <h1 className="heading">Markdown Previewer</h1>
       <div className="content">
         <textarea
